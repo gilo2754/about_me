35f0dfee94cc433ad65f83dbce4b005058e42181
gilo2754 <gilomenjivar@hotmail.com>
2021-01-19 13:46:28 +0100

    done

--- js/main.js
+++ js/main.js
@@ -6,10 +6,6 @@ function myFunctionToHide() {
     x.style.display = "none";
   }*/
   x.style.display = x.style.display === 'block' ? 'none':'block' ;
-
-
-
-
 }
 
 $('.interactive-menu-button a').click(function() {
@@ -73,4 +69,30 @@ $('#blog-btn').click(function() {
 });
 
 
+//Count-Down for TH Köln WI
+var countDownDate = new Date("Aug 15, 2021 15:37:25").getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="decountDown"
+  document.getElementById("countDown").innerHTML = days + "d " + hours + "h "  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countDown").innerHTML = "Graduated";
+  }
+}, 1000);
 
