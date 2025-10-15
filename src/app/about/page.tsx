9092b66d28e81dd3ab8f868034007516de4d7f0b
gilo2754 <gilo2754@gmail.com>
2025-10-15 22:20:19 +0200

    fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { FiCode, FiDatabase, FiCloud, FiSmartphone, FiUsers, FiTarget, FiZap } from 'react-icons/fi';
+import { FiCode, FiDatabase, FiCloud, FiSmartphone, FiUsers, FiTarget, FiZap, FiShield } from 'react-icons/fi';
 
 const teamMembers = [
   {
